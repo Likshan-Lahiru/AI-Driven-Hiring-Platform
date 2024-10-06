3ef2261b2ecf51adf3edfa61f0edf1a9383276b8
lahiruileperumage <lahiru212001@gmail.com>
2024-10-06 14:21:19 +0530

    same component repeated multiple time we want assign key of array valu uniqu

--- src/JobSection.jsx
+++ src/JobSection.jsx
@@ -1,5 +1,5 @@
-import { Card, CardHeader,CardContent,CardTitle,CardFooter} from "@/components/ui/card";
-import { Briefcase, MapPin} from "lucide-react";
+
+import JobCard from "./jobCard";
 function JobSection() {
     
     const jobs = [
@@ -26,28 +26,13 @@ function JobSection() {
    
 
     return ( 
-    <section className="py-8 grid gap-y-4">
+    <section className="py-8 ">
         <h2>Available Jobs</h2>
-        <div className="mt-4 flex-col gap-y-8">
+        <div className="mt-4 flex flex-col gap-y-8">
         {
             jobs.map((job) => {
                 return (
-                    <Card>
-                      <CardHeader>
-                        <CardTitle>{job.title}</CardTitle>
-                      </CardHeader>
-                      <CardContent></CardContent>
-                      <CardFooter className="gap-x-4">
-                        <div className="flex items-center gap-x-2">
-                          <Briefcase />
-                          <span>{job.type}</span>
-                        </div>
-                        <div className="flex items-center gap-x-2">
-                          <MapPin />
-                          <span>{job.location}</span>
-                        </div>
-                      </CardFooter>
-                    </Card>
+                    <JobCard keys = {job._id} job = {job }/>
                 );  
         })}    
         </div>
