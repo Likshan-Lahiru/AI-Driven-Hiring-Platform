a11bbd5fda780f5e5457f68f7eaf62358a1dac48
lahiruileperumage <lahiru212001@gmail.com>
2024-10-07 14:05:24 +0530

    add label component shadcn

--- src/pages/home/components/JobSection.jsx
+++ src/pages/home/components/JobSection.jsx
@@ -7,25 +7,45 @@ function JobSection() {
             _id: "xyz",
             title: "Intern - Software Engineer",
             type: "Full-time",
-            location: "Remote"
+            location: "Remote",
+            questions: [
+                "Share your academic background and highlight key programming concepts you've mastered. How has your education shaped your current tech skill set ?",
+                "Describe your professional development, emphasizing any certifications obtained. How have these certifications enriched your technical abilities, and can you provide an example of their practical application ?",
+                "Discuss notable projects in your programming experience. What challenges did you face, and how did you apply your skills to overcome them? Highlight the technologies used and the impact of these projects on your overall growth as a prefessional ?",
+              ],
         },
         {
             _id: "abc",
             title: "Software Engineer",
             type: "Full Time",
-            location: "Colombo, Sri Lanka"
+            location: "Colombo, Sri Lanka",
+            questions: [
+                "Share your academic background and highlight key programming concepts you've mastered. How has your education shaped your current tech skill set ?",
+                "Describe your professional development, emphasizing any certifications obtained. How have these certifications enriched your technical abilities, and can you provide an example of their practical application ?",
+                "Discuss notable projects in your programming experience. What challenges did you face, and how did you apply your skills to overcome them? Highlight the technologies used and the impact of these projects on your overall growth as a prefessional ?",
+              ],
         },
         {
             _id: "efg",
             title: "DevOps Engineer",
             type: "Full Time",
-            location: "Panadura, Sri Lanka"
+            location: "Panadura, Sri Lanka",
+            questions: [
+                "Share your academic background and highlight key programming concepts you've mastered. How has your education shaped your current tech skill set ?",
+                "Describe your professional development, emphasizing any certifications obtained. How have these certifications enriched your technical abilities, and can you provide an example of their practical application ?",
+                "Discuss notable projects in your programming experience. What challenges did you face, and how did you apply your skills to overcome them? Highlight the technologies used and the impact of these projects on your overall growth as a prefessional ?",
+              ],
         },
         {
             _id: "new4",
             title: "Project Manager",
             type: "Full Time",
-            location: "Kalutara"
+            location: "Kalutara",
+            questions: [
+                "Share your academic background and highlight key programming concepts you've mastered. How has your education shaped your current tech skill set ?",
+                "Describe your professional development, emphasizing any certifications obtained. How have these certifications enriched your technical abilities, and can you provide an example of their practical application ?",
+                "Discuss notable projects in your programming experience. What challenges did you face, and how did you apply your skills to overcome them? Highlight the technologies used and the impact of these projects on your overall growth as a prefessional ?",
+              ],
         }
     ];
 
--- src/pages/job/job.page.jsx
+++ src/pages/job/job.page.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { Briefcase, MapPin } from "lucide-react";
 import Navigation from "@/components/shared/Navigation";
+import { Separator } from "@radix-ui/react-separator";
 
 function JobPage() {
     const jobs = [
@@ -8,25 +9,45 @@ function JobPage() {
             _id: "xyz",
             title: "Intern - Software Engineer",
             type: "Full-time",
-            location: "Remote"
+            location: "Remote",
+            questions: [
+                "Share your academic background and highlight key programming concepts you've mastered. How has your education shaped your current tech skill set ?",
+                "Describe your professional development, emphasizing any certifications obtained. How have these certifications enriched your technical abilities, and can you provide an example of their practical application ?",
+                "Discuss notable projects in your programming experience. What challenges did you face, and how did you apply your skills to overcome them? Highlight the technologies used and the impact of these projects on your overall growth as a prefessional ?",
+              ],
         },
         {
             _id: "abc",
             title: "Software Engineer",
             type: "Full Time",
-            location: "Colombo, Sri Lanka"
+            location: "Colombo, Sri Lanka",
+            questions: [
+                "Share your academic background and highlight key programming concepts you've mastered. How has your education shaped your current tech skill set ?",
+                "Describe your professional development, emphasizing any certifications obtained. How have these certifications enriched your technical abilities, and can you provide an example of their practical application ?",
+                "Discuss notable projects in your programming experience. What challenges did you face, and how did you apply your skills to overcome them? Highlight the technologies used and the impact of these projects on your overall growth as a prefessional ?",
+              ],
         },
         {
             _id: "efg",
             title: "DevOps Engineer",
             type: "Full Time",
-            location: "Panadura, Sri Lanka"
+            location: "Panadura, Sri Lanka",
+            questions: [
+                "Share your academic background and highlight key programming concepts you've mastered. How has your education shaped your current tech skill set ?",
+                "Describe your professional development, emphasizing any certifications obtained. How have these certifications enriched your technical abilities, and can you provide an example of their practical application ?",
+                "Discuss notable projects in your programming experience. What challenges did you face, and how did you apply your skills to overcome them? Highlight the technologies used and the impact of these projects on your overall growth as a prefessional ?",
+              ],
         },
         {
             _id: "new4",
             title: "Project Manager",
             type: "Full Time",
-            location: "Kalutara"
+            location: "Kalutara",
+            questions: [
+                "Share your academic background and highlight key programming concepts you've mastered. How has your education shaped your current tech skill set ?",
+                "Describe your professional development, emphasizing any certifications obtained. How have these certifications enriched your technical abilities, and can you provide an example of their practical application ?",
+                "Discuss notable projects in your programming experience. What challenges did you face, and how did you apply your skills to overcome them? Highlight the technologies used and the impact of these projects on your overall growth as a prefessional ?",
+              ],
         }
     ];
 
@@ -63,6 +84,7 @@ function JobPage() {
       <div className="mt-4 py-4">
         <p>{job.description}</p>
       </div>
+      <Separator/>
         </main>
      );
 }
